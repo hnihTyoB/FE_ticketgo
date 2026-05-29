3f561501be0c1f915c136a8ecc015e6e568feeee
hnihTyoB <nctmdt@gmail.com>
2026-05-29 14:42:15 +0700

    fix: search categogy

--- src/components/Layouts/Client/CategoryFilterBar.tsx
+++ src/components/Layouts/Client/CategoryFilterBar.tsx
@@ -8,7 +8,7 @@ type CategoryFilterBarProps = {
 const CategoryFilterBar = ({ data }: CategoryFilterBarProps) => {
   const navigate = useNavigate();
 
-  const handleCategoryClick = (title: string, catId?: string) => {
+  const handleCategoryClick = (catId?: string) => {
     navigate(`/search?category=${catId ? encodeURIComponent(catId) : ""}`);
   };
 
@@ -18,7 +18,7 @@ const CategoryFilterBar = ({ data }: CategoryFilterBarProps) => {
         {data.map((da) => (
           <button
             key={da.id}
-            onClick={() => handleCategoryClick(da.label, da.id)}
+            onClick={() => handleCategoryClick(da.id)}
             className="font-medium py-6 text-white hover:text-[#2dc275] transition-colors"
           >
             {da.label}
